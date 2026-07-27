dd8b2df13cf00e5f3a7f861c94507e0fb68a0b87
ahmedali109 <ahmedalinaguib33@gmail.com>
2026-07-27 11:14:25 +0300

    Add Input support Region & end Region

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,26 +1,35 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-       // ءVariables and Data Types
-//        int age = 21;
-//        double salary = 2000.0;
-//        char grade = 'A';
-//        boolean graduated = true;
-//        String name = "Ahmed";
-//        IO.println("""
-//Hello, my name is %s and I am %d.
-//I am currently %s.
-//I got %s in my last exam.
-//I hope I get a salary of %.2f.
-//""".formatted(
-//                name,
-//                age,
-//                graduated ? "graduated" : "not graduated",
-//                grade,
-//                salary
-//        ));
+        //region Varibales and Data Types
+        //        int age = 21;
+        //        double salary = 2000.0;
+        //        char grade = 'A';
+        //        boolean graduated = true;
+        //        String name = "Ahmed";
+        //        IO.println("""
+        //Hello, my name is %s and I am %d.
+        //I am currently %s.
+        //I got %s in my last exam.
+        //I hope I get a salary of %.2f.
+        //""".formatted(
+        //                name,
+        //                age,
+        //                graduated ? "graduated" : "not graduated",
+        //                grade,
+        //                salary
+        //        ));
+        //endregion
+
+        //region Input
+        boolean isLoggedIn = true;
+        String name = IO.readln("Enter your name: ");
+        IO.println(
+                """
+                %s%s
+                """.formatted(isLoggedIn ? "Welcome Back , " : "Please Login first , " , name)
+        );
+        //endregion
     }
 }
