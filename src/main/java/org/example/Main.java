3eb3c687a573cec5abe39ff14db46b165250c8f8
ahmedali109 <ahmedalinaguib33@gmail.com>
2026-07-27 10:56:34 +0300

    Variables & Data Types

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,23 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-       IO.println("Hello World");
+       // ءVariables and Data Types
+//        int age = 21;
+//        double salary = 2000.0;
+//        char grade = 'A';
+//        boolean graduated = true;
+//        String name = "Ahmed";
+//        IO.println("""
+//Hello, my name is %s and I am %d.
+//I am currently %s.
+//I got %s in my last exam.
+//I hope I get a salary of %.2f.
+//""".formatted(
+//                name,
+//                age,
+//                graduated ? "graduated" : "not graduated",
+//                grade,
+//                salary
+//        ));
     }
 }
