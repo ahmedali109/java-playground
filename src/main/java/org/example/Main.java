24bc09f3b5e8da1d3e7166aa9f23196338c1a683
ahmedali109 <ahmedalinaguib33@gmail.com>
2026-07-28 00:53:12 +0300

    Collections

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,21 @@
 package org.example;
 
+import java.util.*;
+
 public class Main {
     //region Methods
-    static int Square(int x){
-        return x*x;
-    }
-
-    static int Cube(int x){
-        return x*x*x;
-    }
-
-    static int Factorial(int x){
-        if(x == 1 || x == 0) return 1;
-        return x * Factorial(x - 1);
-    }
+//    static int Square(int x){
+//        return x*x;
+//    }
+//
+//    static int Cube(int x){
+//        return x*x*x;
+//    }
+//
+//    static int Factorial(int x){
+//        if(x == 1 || x == 0) return 1;
+//        return x * Factorial(x - 1);
+//    }
     //endregion
 
     static void main() {
@@ -156,38 +158,150 @@ public class Main {
 //        }
         //endregion
 
-        //region OOP
-        Car c1 = new Car();
-        c1.start();
-        c1.stop();
+        //region Collections
+
+        //region ArrayList
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("Ahmed");
+        arrayList.add("Aly");
+        arrayList.set(0 , "Mohamed");
+        IO.println(arrayList.get(0));
         IO.println("------------------------");
-        Dog d1 = new Dog();
-        d1.name = "Rock";
-        d1.eat();
+        for(String name : arrayList){
+            IO.println("%s".formatted(name));
+        }
         IO.println("------------------------");
-        Rectangle r1 = new Rectangle();
-        r1.draw();
+        int size = arrayList.size();
+        IO.println(size);
         IO.println("------------------------");
-        Trinagle t1 = new Trinagle();
-        t1.draw();
+        arrayList.remove("Mohamed");
+//        arrayList.remove(0);
+        size = arrayList.size();
+        IO.println(size);
         IO.println("------------------------");
-        Calculator calc = new Calculator();
-        int result = calc.add(5 , 4);
-        int total = calc.add(5 , 4 , 3);
-        IO.println("%s %s".formatted(result  , total));
+        IO.println(arrayList.contains("Mohamed"));
         IO.println("------------------------");
-        Developer dev = new Developer();
-        dev.calculateSalary();
-        IO.println("------------------------");
-        Manager m1 = new Manager();
-        m1.calculateSalary();
-        IO.println("------------------------");
-        VodafoneCash vc = new VodafoneCash();
-        vc.pay();
-        IO.println("------------------------");
-        Visa visa = new Visa();
-        visa.pay();
+        arrayList.clear();
+        IO.println(arrayList.isEmpty());
         IO.println("------------------------");
         //endregion
+
+        //region LinkedList
+        List<String> names = new LinkedList<>();
+        names.add("Ali");
+        names.add("Ahmed");
+        names.add("Sara");
+        names.addFirst("Omar");
+        names.addLast("Mona");
+        for(String name : names){
+            IO.println("%s".formatted(name));
+        }
+        IO.println("------------------------");
+        names.removeFirst();
+        names.removeLast();
+        for(String name : names){
+            IO.println("%s".formatted(name));
+        }
+        IO.println("------------------------");
+        //endregion
+
+        //region Set
+        Set<Integer> numbers = new HashSet<Integer>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(10);
+        for(int num : numbers){
+            IO.println("%d".formatted(num));
+        }
+        IO.println("------------------------");
+        Iterator it = numbers.iterator();
+        while (it.hasNext()){
+            IO.println("%d".formatted(it.next()));
+        }
+        IO.println("------------------------");
+        Set<Integer> linkedNumbers = new LinkedHashSet<Integer>();
+        linkedNumbers.add(10);
+        linkedNumbers.add(20);
+        linkedNumbers.add(10);
+        for(int num : linkedNumbers){
+            IO.println("%d".formatted(num));
+        }
+        IO.println("------------------------");
+        Iterator it2 = linkedNumbers.iterator();
+        while (it2.hasNext()){
+            IO.println("%d".formatted(it2.next()));
+        }
+        IO.println("------------------------");
+
+        Set<Integer> tree = new TreeSet<Integer>();
+        tree.add(50);
+        tree.add(10);
+        tree.add(30);
+        tree.add(20);
+        for(int num : tree){
+            IO.println("%d".formatted(num));
+        }
+        IO.println("------------------------");
+        Iterator it3 = tree.iterator();
+        while (it3.hasNext()){
+            IO.println("%d".formatted(it3.next()));
+        }
+        IO.println("------------------------");
+        //endregion
+
+        //region Map
+        Map<Integer , String> students = new HashMap<>();
+        students.put(100 , "Ahmed");
+        students.put(101 , "Mohamed");
+        students.put(102 , "Hassan");
+        IO.println(students.containsKey(100));
+        IO.println(students.containsValue("Mohamed"));
+        IO.println("------------------------");
+        students.remove(102);
+        IO.println(students.keySet());
+        IO.println(students.values());
+        IO.println("------------------------");
+        for(Map.Entry<Integer , String> entry : students.entrySet()){
+            IO.println("%d -> %s".formatted(entry.getKey(),entry.getValue()));
+        }
+        IO.println("------------------------");
+        //endregion
+
+        //endregion
+
+        //region OOP
+//        Car c1 = new Car();
+//        c1.start();
+//        c1.stop();
+//        IO.println("------------------------");
+//        Dog d1 = new Dog();
+//        d1.name = "Rock";
+//        d1.eat();
+//        IO.println("------------------------");
+//        Rectangle r1 = new Rectangle();
+//        r1.draw();
+//        IO.println("------------------------");
+//        Trinagle t1 = new Trinagle();
+//        t1.draw();
+//        IO.println("------------------------");
+//        Calculator calc = new Calculator();
+//        int result = calc.add(5 , 4);
+//        int total = calc.add(5 , 4 , 3);
+//        IO.println("%s %s".formatted(result  , total));
+//        IO.println("------------------------");
+//        Developer dev = new Developer();
+//        dev.calculateSalary();
+//        IO.println("------------------------");
+//        Manager m1 = new Manager();
+//        m1.calculateSalary();
+//        IO.println("------------------------");
+//        VodafoneCash vc = new VodafoneCash();
+//        vc.pay();
+//        IO.println("------------------------");
+//        Visa visa = new Visa();
+//        visa.pay();
+//        IO.println("------------------------");
+        //endregion
+
     }
 }
